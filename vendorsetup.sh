faa666857a3482588a4b700e0ff3df316a322cfa
Asif <najmulhasan3609@gmail.com>
2023-02-21 14:57:44 +0000

    jasmine_sprout: update env

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,9 @@
 add_lunch_combo omni_jasmine_sprout-user
 add_lunch_combo omni_jasmine_sprout-userdebug
 add_lunch_combo omni_jasmine_sprout-eng
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export LC_ALL="C"
+export OF_AB_DEVICE=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+export OF_FORCE_PREBUILT_KERNEL=1
